8b78c3d7ca9411494dbe5e02c8774cc050ea5ea9
Helielton Fioramonte <helifiora@gmail.com>
2021-01-25 19:19:51 -0300

    5a

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template, request, session, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my super key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flasky'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self) -> str:
+        return f'<Role {self.name}>'
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self) -> str:
+        return f'<User {self.username}>'
 
 
 class NameForm(Form):
@@ -12,7 +39,6 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
     if request.method == 'POST':
         form = NameForm(request.form)
         if form.validate():
