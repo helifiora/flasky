0f89081d59b8c4c2d97c181805d048deb346912f
Helielton Fioramonte <helifiora@gmail.com>
2021-01-24 22:21:59 -0300

    3c

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ def user(name: str):
     return render_template('user.html', name=name)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html')
+
+
 app.add_url_rule('/', 'index', index)
 app.add_url_rule('/user/<string:name>', 'user', user)
